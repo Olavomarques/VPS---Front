ec81a946fa57699f01a415baf486eef766372e0d
Olavomarques <161066559+Olavomarques@users.noreply.github.com>
2025-03-28 13:36:41 -0300

    Add files via upload

---
+++ prova-pfe/produtos.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const modal = document.getElementById("modal");
+    const openModal = document.getElementById("openModal");
+    const closeModal = document.getElementById("closeModal");
+
+    openModal.addEventListener("click", function() {
+        modal.style.display = "flex";
+    });
+
+    closeModal.addEventListener("click", function() {
+        modal.style.display = "none";
+    });
+
+    window.addEventListener("click", function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+});
